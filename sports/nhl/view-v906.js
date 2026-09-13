3a0b22f6546627dbee457d6ba931002a5d3a38d3
jthomas0786 <jthomas0786@gmail.com>
2026-09-12 19:27:15 -0500

    Wire NHL player modal into slate and props

--- sports/nhl/view-v906.js
+++ sports/nhl/view-v906.js
@@ -1,10 +1,12 @@
 import * as base from './view.js?v=90.5';
 import {installNhlSlateV906} from './slate-v906.js?v=90.6.1';
+import {installNhlPlayerModalV907} from './player-modal-v907.js?v=90.7';
 import {gradeForLean,gradeRingHTML} from './grade.js?v=90.4';
 
 export const selectTab=base.selectTab;
 export async function mount(){
  const result=await base.mount();
  installNhlSlateV906({gradeForLean,gradeRingHTML});
+ installNhlPlayerModalV907(document.getElementById('nhlView'));
  return result;
 }
