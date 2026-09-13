854839ca3fc550f1fd0e1ca36a2dddc45f92d641
jthomas0786 <jthomas0786@gmail.com>
2026-09-12 19:13:46 -0500

    Bump NHL slate continuation cache

--- sports/nhl/view-v906.js
+++ sports/nhl/view-v906.js
@@ -1,5 +1,5 @@
 import * as base from './view.js?v=90.5';
-import {installNhlSlateV906} from './slate-v906.js?v=90.6';
+import {installNhlSlateV906} from './slate-v906.js?v=90.6.1';
 import {gradeForLean,gradeRingHTML} from './grade.js?v=90.4';
 
 export const selectTab=base.selectTab;
